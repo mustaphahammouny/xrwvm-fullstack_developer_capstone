2098ababbb0f1b56ea6413ce380a25319c1d8880
mustapha <mustapha.hammouny@gmail.com>
2025-01-19 11:28:41 -0500

    feat: cars api

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,12 +30,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    #'https://mustaphahamm-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://mustaphahamm-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://mustaphahamm-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://mustaphahamm-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://mustaphahamm-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 CSRF_TRUSTED_ORIGINS = [
-    #'https://mustaphahamm-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://mustaphahamm-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://mustaphahamm-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://mustaphahamm-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://mustaphahamm-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 REST_FRAMEWORK = {
