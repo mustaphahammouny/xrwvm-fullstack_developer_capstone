8a88756119ba5e89bbc32e359f62e578c792911d
mustapha <mustapha.hammouny@gmail.com>
2025-01-19 15:31:50 -0500

    fix: lint python

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,14 +30,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://mustaphahamm-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://mustaphahamm-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://mustaphahamm-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    ('https://mustaphahamm-8000.theianext-0-labs-prod-misc-tools-'
+    'us-east-0.proxy.cognitiveclass.ai'),
+    ('https://mustaphahamm-8000.theianext-1-labs-prod-misc-tools-'
+    'us-east-0.proxy.cognitiveclass.ai'),
+    ('https://mustaphahamm-8000.theiadockernext-1-labs-prod-theiak8s-4-'
+    'tor01.proxy.cognitiveclass.ai'),
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://mustaphahamm-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://mustaphahamm-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://mustaphahamm-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    ('https://mustaphahamm-8000.theianext-0-labs-prod-misc-tools-'
+    'us-east-0.proxy.cognitiveclass.ai'),
+    ('https://mustaphahamm-8000.theianext-1-labs-prod-misc-tools-'
+    'us-east-0.proxy.cognitiveclass.ai'),
+    ('https://mustaphahamm-8000.theiadockernext-1-labs-prod-theiak8s-4-'
+    'tor01.proxy.cognitiveclass.ai'),
 ]
 
 REST_FRAMEWORK = {
@@ -103,19 +109,23 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        ('django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator'),
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        ('django.contrib.auth.password_validation.'
+        'MinimumLengthValidator'),
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        ('django.contrib.auth.password_validation.'
+        'CommonPasswordValidator'),
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        ('django.contrib.auth.password_validation.'
+        'NumericPasswordValidator'),
     },
 ]
 
@@ -152,4 +162,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static')
 ]
-
